127c5f4850d7d978be3ed1512989fce362696692
S7L7 <buijohn@seznam.cz>
2025-12-30 22:27:52 +0100

    base code,database

--- DataFetch.cpp
+++ DataFetch.cpp
@@ -1,9 +1,5 @@
-//
-// Created by bui on 29.11.2025.
-//
 #include <iostream>
-#include <string>
-#include "sqlite3.h"
+#include "DataFetch.h"
 
 using namespace std;
 
@@ -14,7 +10,7 @@ sqlite3* openDatabase(const string &filename) {
         return nullptr;
     }
     return db;
-    };
+    }
 
 int findemployeeId(sqlite3* db, const string& rfid) {
     const char* sql= "SELECT id FROM employees WHERE rfid_uid = ? LIMIT 1;";
@@ -48,7 +44,7 @@ bool insertAttendance(sqlite3* db,int employeeId, const string &type) {
             return false;
         }
     sqlite3_bind_int(stmt, 1, employeeId);
-    sqlite3_bind_text(stmt, 2, type.c_str(), -1, nullptr);
+    sqlite3_bind_text(stmt, 2, type.c_str(), -1, SQLITE_TRANSIENT);
     int rc=sqlite3_step(stmt);
 
     if (rc != SQLITE_DONE) {
@@ -87,42 +83,4 @@ sqlite3_bind_int (stmt, 1 , employeeId);
 
     sqlite3_finalize(stmt);
     return result;
-}
-
-int main() {
-        sqlite3* db = openDatabase("../attendance.db");
-        if (!db) return 1;
-    while (true) {
-        string rfid;
-        cout << "Zadejte ID: ";
-        cin >> rfid;
-
-        if (rfid == "EXIT") {
-            break;
-        }
-
-
-
-
-        int employeeId = findemployeeId(db, rfid);
-
-        if (employeeId == -1) {
-            cout << "Zamestnanec nenalezen" << endl;
-            sqlite3_close(db);
-            return 1;
-        }
-
-        cout << "Zamestnanec nalezen s prirazenym ID: " << employeeId << endl;
-
-        string type = nextEventType(db,employeeId);
-        cout << "Entry/Exit: " << type << endl;
-
-        if (insertAttendance(db,employeeId,type)) {
-            cout << "Dochazka zapsana" << endl;
-        } else {
-            cout << "Error: Nezapsano" << endl;
-        }
-    }
-        sqlite3_close(db);
-        return 0;
 }
