085a02d757be807a30f768c97ea34aec58413704
S7L7 <buijohn@seznam.cz>
2025-12-30 22:27:59 +0100

    base code,database

---
+++ Attendance_log.h
@@ -0,0 +1,13 @@
+//
+// Created by buijo on 30.12.2025.
+//
+
+#ifndef DOCHAZKOVY_SYSTEM_ATTENDANCE_LOG_H
+#define DOCHAZKOVY_SYSTEM_ATTENDANCE_LOG_H
+#include <string>
+
+void writeAttendanceToFile(
+const std::string &employeename,
+const std::string &type
+);
+#endif //DOCHAZKOVY_SYSTEM_ATTENDANCE_LOG_H
---
+++ DataFetch.h
@@ -0,0 +1,17 @@
+//
+// Created by buijo on 30.12.2025.
+//
+
+#ifndef DOCHAZKOVY_SYSTEM_DATAFETCH_H
+#define DOCHAZKOVY_SYSTEM_DATAFETCH_H
+
+#include <string>
+#include "sqlite3.h"
+
+
+sqlite3* openDatabase(const std::string &filename);
+int findemployeeId(sqlite3* db, const std::string& rfid);
+bool insertAttendance(sqlite3* db,int employeeId, const std::string &type);
+std::string nextEventType(sqlite3* db, int employeeId);
+
+#endif //DOCHAZKOVY_SYSTEM_DATAFETCH2_H
