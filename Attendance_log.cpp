540cf94cf5bb55e4c120a8b8b80484c932fd7b51
S7L7 <buijohn@seznam.cz>
2025-12-30 22:27:59 +0100

    base code,database

---
+++ Attendance_log.cpp
@@ -0,0 +1,35 @@
+//
+// Created by buijo on 30.12.2025.
+//
+#include "Attendance_log.h"
+#include <fstream>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+static string getCurrentTimeStamp() {
+    time_t now = time(nullptr);
+    tm* ltm = localtime(&now);
+
+    char buffer[20];
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M", ltm);
+    return string(buffer);
+}
+void writeAttendanceToFile(const string& employeeName, const string& type) {
+    ofstream file("attendance_log.txt", ios::app);
+
+    if (!file.is_open()) {
+        cerr << "Can't open attendance_log.txt" << endl;
+        return;
+    }
+
+    file <<getCurrentTimeStamp()
+         << " | "
+         << employeeName
+         << " | "
+         << type
+         << endl;
+
+    file.close();
+}
