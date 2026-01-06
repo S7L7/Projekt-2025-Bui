f0457d98e1ce7a4136e5afa37522e336b355d793
S7L7 <buijohn@seznam.cz>
2026-01-06 12:42:33 +0100

    LVL timer func

--- main.cpp
+++ main.cpp
@@ -16,9 +16,11 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8); // chcp 65001
 
-        sqlite3* db = openDatabase("../attendance.db");
-        if (!db) return 1;
-    std::unordered_map<int,Clock::time_point> LastValidAction;
+    sqlite3* db = openDatabase("../attendance.db");
+    if (!db) return 1;
+
+    std::unordered_map<int,Clock::time_point> lastValidAction;
+
     while (true) {
         string rfid;
         cout << "Zadejte ID: ";
@@ -39,52 +41,38 @@ int main() {
 
         auto now = Clock::now();
 
-        auto it = LastValidAction.find(emp.id);
-        if (it != LastValidAction.end()) {
-            auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(now - it->second).count();
-            if (diff < 10) {
-                cerr << "Neplatný vstup(počkejte 10 vteřin)" << endl;
-                continue;
-            }
-        }
-        auto now = Clock::now();
-
         auto it = lastValidAction.find(emp.id);
         if (it != lastValidAction.end()) {
             auto diff = std::chrono::duration_cast<std::chrono::seconds>(now - it->second).count();
 
             if (diff < 10) {
-                std::cerr << "Invalid log (too fast)" << std::endl;
-                continue; // NIC se nezapíše, stav se nemění
+                    cerr << "Neplatný vstup(počkejte 10 vteřin)" << endl;
+                    continue;
+                }
             }
-        }
 
         string type;
-        if (emp.status == 0) {
+        if (emp.status == 0)
             type = "Entry";
-        }else {
+        else
             type = "Exit";
-        }
-        auto LastValid = getCurrentTime();
-        std::cout << "Start Time: " << startTime.time_since_epoch().count() << "\n";
-        std::cout << (getCurrentTime() - startTime).count() << "\n";
-    }
-        cout << "Entry/Exit" << type << endl;
-        string employeeName = getEmployeeName(db, emp.id);
+
+        cout << "Entry/Exit: " << type << endl;
 
         if (insertAttendance(db, emp.id, type)) {
             int newStatus = (type == "entry") ? 1:0;
-            updateEmployeeStatus(db, emp.id, newStatus);
+            if (updateEmployeeStatus(db, emp.id, newStatus)) {
+                lastValidAction[emp.id] = now;
+            }
 
             cout << "Dochazka zapsana" << endl;
 
             writeAttendanceToFile(
-            to_string(emp.id)+ " | " + employeeName,
-                type);
-        } else {
-            cout << "Error: Nezapsano" << endl;
+            to_string(emp.id)+ " | " + emp.name, type);
+            } else {
+                cout << "Error: Nezapsano" << endl;
+            }
         }
-    }
         sqlite3_close(db);
         return 0;
 }
